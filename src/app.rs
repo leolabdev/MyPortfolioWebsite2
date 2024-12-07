dd930bcc4b19df8c43332d56f8041d8815c1fc53
leoha <s@>
2024-12-07 20:04:26 +0200

    Add Tailwind CSS and Stylist for styling support
    Introduce Tailwind CSS setup by creating `output.css` and configuration in `tailwind.config.js` for streamlined styling across HTML and Rust files. Additionally, integrate the `stylist` library in the Rust project to manage scoped styles alongside Yew, allowing for enhanced and organized CSS management. Update to include necessary style links and dependencies in the HTML and Cargo files.

---
+++ src/app.rs
@@ -0,0 +1,26 @@
+use yew::prelude::*;
+
+#[function_component]
+pub fn App() -> Html {
+    html! {<div>{"Hello World!"}</div>}
+}
+
+
+
+// let counter = use_state(|| 0);
+// let onclick = {
+//     let counter = counter.clone();
+//     move |_| {
+//         let value = *counter + 1;
+//         counter.set(value);
+//     }
+// };
+//
+// html! {
+//     <div>
+//         <button {onclick}>{ "+1" }</button>
+//         <p>{ *counter }</p>
+//     </div>
+// }
+
+// let style = use_style!("color: red;");
