29e09644d7a687176a08cb2e3e234a3e88d78bed
leoha <s@>
2024-12-08 20:14:55 +0200

    Improve contact text layout for better readability
    The contact information text is now split into two lines to enhance readability and visual appeal. This change ensures that the message is easily clear and aesthetically pleasing when viewed by users.

--- src/components/contact.rs
+++ src/components/contact.rs
@@ -10,7 +10,11 @@ pub fn contact() -> Html {
                         {"Contact me"}
                     </b>
                 </h2>
-                <p class="text-2xl  font-sans font-extralight">{"I'm always interested in hearing about new studying or working opportunities."}</p>
+                <p class="text-xl  font-sans font-extralight">
+                     {"I'm always interested in hearing about new studying or working"}
+                    <br />
+                    {"opportunities."}
+                </p>
                 <a href="mailto:leohannolainen999@gmail.com" class="mt-8 text-xl  inline-block py-2 px-2 rounded bg-custom-special hover:bg-custom-active">
                 {"Email me"}
                 </a>
