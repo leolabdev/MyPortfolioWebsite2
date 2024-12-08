533d3c657a02f705b887042d89c716b4a89212bb
leoha <s@>
2024-12-08 18:37:29 +0200

    Add tighter letter-spacing and bold in nav header
    This commit introduces new CSS classes to adjust letter-spacing (tracking) in the stylesheet, allowing for finer control over text presentation. Additionally, the navigation header text is updated to use a font-sans with a tighter letter spacing and bold

--- src/components/nav.rs
+++ src/components/nav.rs
@@ -6,10 +6,13 @@ pub fn nav() -> Html {
         <nav class="bg-custom-bg-add text-custom-main text-lg font-mono py-16 px-4">
             <ul class="flex flex-wrap justify-center items-center gap-8 max-w-[1200px] mx-auto">
                 <li class="w-full text-center lg:w-auto lg:text-left lg:mr-auto">
-                    <h1 class="text-2xl group hover:text-custom-active inline-flex font-medium">
+                    <h1 class="text-2xl group hover:text-custom-active inline-flex font-sans tracking-tight">
                         <a href="/" class="">
-                            <span class="fab fa-node-js pr-2 text-custom-add group-hover:text-custom-active" aria-hidden="true"></span>
-                            {"Leo Hannolainen"}
+                           <b class="font-semibold">
+                           <span class="fab fa-node-js pr-2 text-custom-add group-hover:text-custom-active" aria-hidden="true"></span>
+                              {"Leo Hannolainen"}
+                           </b>
+
                         </a>
                     </h1>
                 </li>
