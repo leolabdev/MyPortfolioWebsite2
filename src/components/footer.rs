1703c68c208dd438f329c15844f3f22ef18dc685
leoha <s@>
2024-12-08 20:29:14 +0200

    Enhance footer styling and spacing.
    Expanded footer padding and updated text sizes for improved readability. Added custom color and hover effects to footer icons to enhance visual appeal. Adjusted spacing and styling properties in CSS for more consistent layout across elements.

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -3,26 +3,26 @@ use yew::prelude::*;
 #[function_component(Footer)]
 pub fn footer() -> Html {
     html! {
-        <footer class="text-center py-8 text-white">
-            <h2 class="text-xl">{"Leo Hannolainen · Web developer"}</h2>
-            <ul class="flex justify-center gap-4 mt-4 text-2xl">
+        <footer class="text-center py-16 text-custom-main bg-custom-bg-add">
+            <h2 class="text-3xl font-semibold">{"Leo Hannolainen · Web developer"}</h2>
+            <ul class="flex justify-center gap-12 mt-8 text-2xl">
                 <li>
                     <a href="https://linkedin.com/in/leo-hannolainen-860859205" target="_blank" rel="noopener noreferrer">
-                        <span class="fab fa-linkedin"></span>
+                        <span class="fab fa-linkedin hover:text-custom-active text-[200%]"></span>
                     </a>
                 </li>
                 <li>
                     <a href="https://github.com/leolabdev" target="_blank" rel="noopener noreferrer">
-                        <span class="fab fa-github-square"></span>
+                        <span class="fab fa-github-square hover:text-custom-active text-[200%]"></span>
                     </a>
                 </li>
                 <li>
                     <a href="mailto:leohannolainen999@gmail.com">
-                        <span class="fas fa-envelope"></span>
+                        <span class="fas fa-envelope hover:text-custom-active text-[200%]"></span>
                     </a>
                 </li>
             </ul>
-            // <p class="mt-4 text-sm">&copy; 2024 Leo Hannolainen. All rights reserved.</p>
+            <p class="mt-8 text-base">{"© 2024 Leo Hannolainen. All rights reserved."}</p>
         </footer>
     }
 }
